5e887bc6b3c2a595efdae503f2dda810d71cdd0e
Ben Pettit <benpettit@digimulti.com>
2023-04-25 14:11:14 +1000

    control audio vollume on mac with osc server

--- feedback_osc_server.py
+++ feedback_osc_server.py
@@ -1,9 +1,15 @@
 from pythonosc.dispatcher import Dispatcher
 from pythonosc.osc_server import BlockingOSCUDPServer
+import osascript
 
+def set_volume(target_volume):
+    vol = "set volume output volume " + str(target_volume)
+    osascript.osascript(vol)
 
 def print_handler(address, *args):
     print(f"{address}: {args}")
+    target_volume = args
+    set_volume(50)
 
 
 def default_handler(address, *args):
@@ -15,7 +21,7 @@ dispatcher.map("/feedback/*", print_handler)
 dispatcher.set_default_handler(default_handler)
 
 ip = "0.0.0.0"
-port = 32767
+port = 4546
 
 server = BlockingOSCUDPServer((ip, port), dispatcher)
 print("serving")
